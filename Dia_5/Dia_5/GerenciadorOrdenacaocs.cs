c0651a23b3b4ee1515ab793014d7b4a7f9379f17
Pierre Carvalho Fenner <40218734+pierreCarvalho@users.noreply.github.com>
2018-09-18 00:54:38 -0300

    Quinto dia - Ordenando vetor com Bolha

---
+++ Dia_5/Dia_5/GerenciadorOrdenacaocs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dia_5
+{
+    class GerenciadorOrdenacaocs
+    {
+        //exibir uma lista/vetor
+        public static void exibirVetor(int[] vetor)
+        {
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+
+
+        }
+        //popular uma lista/vetor
+        public static void populaVetor(int[] vetor)
+        {
+            Random gerador = new Random();
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = gerador.Next(100); 
+            }
+
+        }
+        //ordenar uma lista/vetor
+        public static void bolha(int[] vetor)
+        {
+
+            bool houveTroca;
+            int tmp = 0;
+            do {
+                houveTroca = false;
+                for (int j = 0; j < vetor.Length - 1; j++)
+                {
+                    if (vetor[j] > vetor[j + 1])
+                    {
+                        tmp = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = tmp;
+                        houveTroca = true;
+                    }
+                }
+            }while(houveTroca);
+
+        }
+    }
+}
---
+++ Dia_5/Dia_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dia_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[50];
+
+            GerenciadorOrdenacaocs.populaVetor(vetor);
+            GerenciadorOrdenacaocs.exibirVetor(vetor);
+            Console.WriteLine("--------------");
+            GerenciadorOrdenacaocs.bolha(vetor);
+            GerenciadorOrdenacaocs.exibirVetor(vetor);
+
+
+
+        }
+    }
+}
