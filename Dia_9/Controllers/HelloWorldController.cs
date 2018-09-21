0b799fbeeea8130f477f1875cdd6cdf45ac65185
Pierre Carvalho Fenner <40218734+pierreCarvalho@users.noreply.github.com>
2018-09-21 18:00:25 -0300

    Nono dia - Introdução à MVC

---
+++ Dia_9/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dia_9.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string HelloUniverse(String nome,int valor = 1)
+        {
+            return HttpUtility.HtmlEncode(" Hello "+ nome+" O valor recebido é "+ valor);
+        }
+    }
+}
