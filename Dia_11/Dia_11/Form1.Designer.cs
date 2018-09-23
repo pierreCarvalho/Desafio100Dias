c0be2050b7fe922c03538b5f80af9f3342952a9f
Pierre Carvalho Fenner <40218734+pierreCarvalho@users.noreply.github.com>
2018-09-23 19:41:38 -0300

    Décimo Primeiro dia - inserindo dados no dgv no c#

---
+++ Dia_11/Dia_11/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Dia_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewCadastrados = new System.Windows.Forms.DataGridView();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxMatricula = new System.Windows.Forms.TextBox();
+            this.textBoxTelefone = new System.Windows.Forms.TextBox();
+            this.LabelNome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonRegistrar = new System.Windows.Forms.Button();
+            this.buttonExcluir = new System.Windows.Forms.Button();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Matricula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCadastrados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCadastrados
+            // 
+            this.dataGridViewCadastrados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCadastrados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nome,
+            this.Matricula,
+            this.Telefone});
+            this.dataGridViewCadastrados.Location = new System.Drawing.Point(176, 12);
+            this.dataGridViewCadastrados.Name = "dataGridViewCadastrados";
+            this.dataGridViewCadastrados.Size = new System.Drawing.Size(343, 150);
+            this.dataGridViewCadastrados.TabIndex = 0;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(13, 35);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNome.TabIndex = 1;
+            // 
+            // textBoxMatricula
+            // 
+            this.textBoxMatricula.Location = new System.Drawing.Point(13, 80);
+            this.textBoxMatricula.Name = "textBoxMatricula";
+            this.textBoxMatricula.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMatricula.TabIndex = 2;
+            // 
+            // textBoxTelefone
+            // 
+            this.textBoxTelefone.Location = new System.Drawing.Point(12, 123);
+            this.textBoxTelefone.Name = "textBoxTelefone";
+            this.textBoxTelefone.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTelefone.TabIndex = 3;
+            // 
+            // LabelNome
+            // 
+            this.LabelNome.AutoSize = true;
+            this.LabelNome.Location = new System.Drawing.Point(13, 13);
+            this.LabelNome.Name = "LabelNome";
+            this.LabelNome.Size = new System.Drawing.Size(35, 13);
+            this.LabelNome.TabIndex = 4;
+            this.LabelNome.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Matricula";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Telefone";
+            // 
+            // buttonRegistrar
+            // 
+            this.buttonRegistrar.Location = new System.Drawing.Point(12, 158);
+            this.buttonRegistrar.Name = "buttonRegistrar";
+            this.buttonRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonRegistrar.TabIndex = 7;
+            this.buttonRegistrar.Text = "Registrar";
+            this.buttonRegistrar.UseVisualStyleBackColor = true;
+            this.buttonRegistrar.Click += new System.EventHandler(this.buttonRegistrar_Click);
+            // 
+            // buttonExcluir
+            // 
+            this.buttonExcluir.Location = new System.Drawing.Point(12, 198);
+            this.buttonExcluir.Name = "buttonExcluir";
+            this.buttonExcluir.Size = new System.Drawing.Size(136, 23);
+            this.buttonExcluir.TabIndex = 8;
+            this.buttonExcluir.Text = "Excluir linha selecionada\r\n\r\n";
+            this.buttonExcluir.UseVisualStyleBackColor = true;
+            this.buttonExcluir.Click += new System.EventHandler(this.buttonExcluir_Click);
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Matricula
+            // 
+            this.Matricula.HeaderText = "Matricula";
+            this.Matricula.Name = "Matricula";
+            // 
+            // Telefone
+            // 
+            this.Telefone.HeaderText = "Telefone";
+            this.Telefone.Name = "Telefone";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 235);
+            this.Controls.Add(this.buttonExcluir);
+            this.Controls.Add(this.buttonRegistrar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LabelNome);
+            this.Controls.Add(this.textBoxTelefone);
+            this.Controls.Add(this.textBoxMatricula);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.dataGridViewCadastrados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCadastrados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCadastrados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Matricula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefone;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxMatricula;
+        private System.Windows.Forms.TextBox textBoxTelefone;
+        private System.Windows.Forms.Label LabelNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonRegistrar;
+        private System.Windows.Forms.Button buttonExcluir;
+    }
+}
+
---
+++ Dia_11/Dia_11/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dia_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRegistrar_Click(object sender, EventArgs e)
+        {
+            String[] dados = { textBoxNome.Text, textBoxMatricula.Text, textBoxTelefone.Text };
+
+
+            //inserir dados na tabela
+
+            dataGridViewCadastrados.Rows.Add(dados);
+
+            textBoxMatricula.Clear();
+            textBoxNome.Clear();
+            textBoxTelefone.Clear();
+
+
+        }
+
+        private void buttonExcluir_Click(object sender, EventArgs e)
+        {
+
+             int index = dataGridViewCadastrados.CurrentRow.Index;
+             dataGridViewCadastrados.Rows.RemoveAt(index);
+            
+        }
+    }
+}
