17cb9393d913f2db8b84d41fc151ce667555733b
Pierre Carvalho Fenner <40218734+pierreCarvalho@users.noreply.github.com>
2018-09-25 00:20:41 -0300

    Décimo Segundo dia - dataGriedView em c#

---
+++ Dia_12/Dia_12/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Dia_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewAgenda = new System.Windows.Forms.DataGridView();
+            this.Data = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnHora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Médico = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Convênio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxData = new System.Windows.Forms.TextBox();
+            this.textBoxHora = new System.Windows.Forms.TextBox();
+            this.checkBoxConvenio = new System.Windows.Forms.CheckBox();
+            this.comboBoxMedico = new System.Windows.Forms.ComboBox();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonAgendar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAgenda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewAgenda
+            // 
+            this.dataGridViewAgenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAgenda.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Data,
+            this.ColumnHora,
+            this.Nome,
+            this.Médico,
+            this.Convênio});
+            this.dataGridViewAgenda.Location = new System.Drawing.Point(12, 191);
+            this.dataGridViewAgenda.Name = "dataGridViewAgenda";
+            this.dataGridViewAgenda.Size = new System.Drawing.Size(545, 150);
+            this.dataGridViewAgenda.TabIndex = 0;
+            // 
+            // Data
+            // 
+            this.Data.HeaderText = "Data";
+            this.Data.Name = "Data";
+            // 
+            // ColumnHora
+            // 
+            this.ColumnHora.HeaderText = "Hora";
+            this.ColumnHora.Name = "ColumnHora";
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Médico
+            // 
+            this.Médico.HeaderText = "Médico";
+            this.Médico.Name = "Médico";
+            // 
+            // Convênio
+            // 
+            this.Convênio.HeaderText = "Convênio";
+            this.Convênio.Name = "Convênio";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(12, 72);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNome.TabIndex = 1;
+            // 
+            // textBoxData
+            // 
+            this.textBoxData.Location = new System.Drawing.Point(118, 70);
+            this.textBoxData.Name = "textBoxData";
+            this.textBoxData.Size = new System.Drawing.Size(100, 20);
+            this.textBoxData.TabIndex = 2;
+            // 
+            // textBoxHora
+            // 
+            this.textBoxHora.Location = new System.Drawing.Point(224, 70);
+            this.textBoxHora.Name = "textBoxHora";
+            this.textBoxHora.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHora.TabIndex = 3;
+            // 
+            // checkBoxConvenio
+            // 
+            this.checkBoxConvenio.AutoSize = true;
+            this.checkBoxConvenio.Location = new System.Drawing.Point(147, 125);
+            this.checkBoxConvenio.Name = "checkBoxConvenio";
+            this.checkBoxConvenio.Size = new System.Drawing.Size(71, 17);
+            this.checkBoxConvenio.TabIndex = 4;
+            this.checkBoxConvenio.Text = "Convênio";
+            this.checkBoxConvenio.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxMedico
+            // 
+            this.comboBoxMedico.FormattingEnabled = true;
+            this.comboBoxMedico.Items.AddRange(new object[] {
+            "Dr. Carlos da Rocha",
+            "Dra. Gabriela mattos"});
+            this.comboBoxMedico.Location = new System.Drawing.Point(12, 123);
+            this.comboBoxMedico.Name = "comboBoxMedico";
+            this.comboBoxMedico.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMedico.TabIndex = 5;
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(12, 56);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(35, 13);
+            this.labelNome.TabIndex = 6;
+            this.labelNome.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Data";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hora";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Médico";
+            // 
+            // buttonAgendar
+            // 
+            this.buttonAgendar.Location = new System.Drawing.Point(16, 162);
+            this.buttonAgendar.Name = "buttonAgendar";
+            this.buttonAgendar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAgendar.TabIndex = 10;
+            this.buttonAgendar.Text = "Agendar";
+            this.buttonAgendar.UseVisualStyleBackColor = true;
+            this.buttonAgendar.Click += new System.EventHandler(this.buttonAgendar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Agendamento de Clientes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 370);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAgendar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.comboBoxMedico);
+            this.Controls.Add(this.checkBoxConvenio);
+            this.Controls.Add(this.textBoxHora);
+            this.Controls.Add(this.textBoxData);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.dataGridViewAgenda);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAgenda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewAgenda;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnHora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Médico;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Convênio;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxData;
+        private System.Windows.Forms.TextBox textBoxHora;
+        private System.Windows.Forms.CheckBox checkBoxConvenio;
+        private System.Windows.Forms.ComboBox comboBoxMedico;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonAgendar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Dia_12/Dia_12/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dia_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAgendar_Click(object sender, EventArgs e)
+        {
+            String convenio = "Não";
+            if (checkBoxConvenio.Checked) convenio = "Sim";
+            String[] dados = { textBoxData.Text, textBoxHora.Text, textBoxNome.Text,comboBoxMedico.Text, convenio };
+
+
+            dataGridViewAgenda.Rows.Add(dados);
+
+            limpaCampos();
+        }
+
+
+        private void limpaCampos()
+        {
+            textBoxData.Clear();
+            textBoxHora.Clear();
+            textBoxNome.Clear();
+            checkBoxConvenio.Checked = false;
+            comboBoxMedico.ResetText();
+
+        }
+    }
+}
